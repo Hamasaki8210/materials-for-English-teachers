b1c821c55dcec68ab3cfc2af93e565551106761d
Ayaka_I_0210 <ayakah2525@gmail.com>
2023-03-21 21:47:25 +0900

    Timeline with three colored backgrounds

--- resources/views/for_teachers/grammar.blade.php
+++ resources/views/for_teachers/grammar.blade.php
@@ -5,27 +5,65 @@
 @section('grammar')
 <div class="grammar-area-outer">
     <div class="grammar-area-inner">
-        <div class="title">Grammar</div>
-        <div class="categories">
-            <div class="category-line">
-                <span class="session">
-                    <div class="img-title img-left"><a href="{{URL::to('/for_teachers/simple_sentences')}}">Simple Sentences</a></div>
-                    <div><a href="{{URL::to('/for_teachers/simple_sentences')}}"><img class="img img-left" src="{{ asset('images/1.jpg') }}" alt=""></a></div>
-                </span>
-                <span class="session">
-                    <div class="img-title img-right">grammar practice worksheets</div>
-                    <div><img class="img img-right" src="{{ asset('images/2.jpg') }}" alt=""></div>
-                </span>
-            </div>
-            <div class="category-line">
-                <span class="session">
-                    <div class="img-title img-left">Simple Sentences</div>
-                    <div><img class="img img-left" src="{{ asset('images/3.jpg') }}" alt=""></div>
-                </span>
-                <span class="session">
-                    <div class="img-title img-right">grammar practice worksheets</div>
-                    <div><img class="img img-right" src="{{ asset('images/4.jpg') }}" alt=""></div>
-                </span>
+        <div class="title">Tenses</div>
+        <div class="container">
+            <div class="row timeline-area">
+                <div class="col past-area">
+                    <div class="tense-area-inner">
+                        <div class="past-area-top area-top">
+                            <div class="balloon-top">Simple Past</div>
+                            <div class="no-balloon"></div>
+                            <div class="balloon-top">Simple Present</div>
+                            <div class="no-balloon"></div>
+                        </div>
+                        <div class="timeline-past-outer">
+                            <div class="timeline-past timeline"></div>
+                        </div>
+                        <div class="past-area-bottom area-bottom">
+                            <div class="no-balloon"></div>
+                            <div class="balloon-bottom">Past Perfect</div>
+                            <div class="no-balloon"></div>
+                            <div class="balloon-bottom balloon-present-perfect">Present Perfect</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col present-area">
+                    <div class="tense-area-inner">
+                        <div class="present-area-top area-top">
+                            <div class="balloon-top">Simple Past</div>
+                            <div class="no-balloon"></div>
+                            <div class="balloon-top">Simple Present</div>
+                            <div class="no-balloon"></div>
+                        </div>
+                        <div class="timeline-present timeline"></div>
+                        <div class="present-area-bottom area-bottom">
+                            <div class="no-balloon"></div>
+                            <div class="balloon-bottom">Past Perfect</div>
+                            <div class="no-balloon"></div>
+                            <div class="balloon-bottom balloon-present-perfect">Present Perfect</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col future-area">
+                    <div class="tense-area-inner">
+                        <div class="future-area-top area-top">
+                            <div class="balloon-top">Simple Past</div>
+                            <div class="no-balloon"></div>
+                            <div class="balloon-top">Simple Present</div>
+                            <div class="no-balloon"></div>
+                        </div>
+                        <div class="timeline-future-outer">
+                            <div class="timeline-future timeline"></div>
+                        </div>
+                        <div class="future-area-bottom area-bottom">
+                            <div class="no-balloon"></div>
+                            <div class="balloon-bottom">Past Perfect</div>
+                            <div class="no-balloon"></div>
+                            <div class="balloon-bottom balloon-present-perfect">Present Perfect</div>
+                            <div class="no-balloon"></div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
