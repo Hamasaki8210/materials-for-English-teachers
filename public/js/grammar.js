cc775c506e9d24f813ec4934583df627c1272b08
Hamasaki8210 <ayakah2525@gmail.com>
2023-04-17 23:14:11 +0900

    pulldowns in progress ver2

--- public/js/grammar.js
+++ public/js/grammar.js
@@ -1,50 +1,23 @@
-$(function(){
-    // $('.js-modal-open').hover(function(){
-    //     $('.js-modal').fadeIn();
-    //     return false;
-    // });
-
-    var timer = "";
-    var count = 0;
-    // $('.js-modal-open').mouseover(
-    //     function(){
-
-    //         $(this).addClass('hovering');
-    //         timer = setInterval(function(){
-    //                 $(document).tooltip({
-    //                     position:{
-    //                         my:"left+30 top",
-    //                         at:"right top",
-    //                         collision:"flipfit"
-    //                     }
-    //                 });
-    //                 count++;
-    //                 if(count === 4){
-    //                     $('.hovering').addClass('opening');
-    //                     $('.hovering').removeClass('hovering');
-    //                     count = 0;
-    //                     clearInterval(timer);
-    //                     $('.js-modal').fadeIn();
-    //                     return false;
-    //                 }
-    //             },100
-    //         );
-    //     }
-    // ).mouseout(
-    //     function(){
-    //         count = 0;
-    //         clearInterval(timer);
-    //     }
-    // )
-    // $('.js-modal-close').on('click',function(){
-    //     var openingClassNum = $('.opening').length;
-    //     console.log(openingClassNum);
-    //     if(openingClassNum > 0){
-    //         // console.log(openingClassNum);
-    //         $('.opening').removeClass('opening');
-    //         $('.js-modal').fadeOut();
-    //         clearInterval(timer);
-    //         return false;
-    //     }
-    // });
-});
+$(function()
+{
+    $('.overlap-3rd').hover(
+        function() {
+            $('.overlap-3rd').css('z-index','1');
+            $('.overlap-4rd').css('z-index','0');
+        },
+        function() {
+            $('.overlap-4rd').css('z-index','1');
+            $('.overlap-3rd').css('z-index','0');
+        }
+    );
+    $('.overlap-4th').hover(
+        function() {
+            $('.overlap-3rd').css('z-index','0');
+            $('.overlap-4rd').css('z-index','1');
+        },
+        function() {
+            $('.overlap-4rd').css('z-index','0');
+            $('.overlap-3rd').css('z-index','1');
+        }
+    );
+})
--- resources/views/for_teachers/grammar.blade.php
+++ resources/views/for_teachers/grammar.blade.php
@@ -1,16 +1,13 @@
 @extends('for_teachers.contents')
 @push('css')
     <link href="{{ asset('css/grammar.css') }}" rel="stylesheet">
-    <link type="text/css" rel="stylesheet"
-  href="http://code.jquery.com/ui/1.13.2/themes/cupertino/jquery-ui.min.css" />
 
 @endpush
 @push('js')
     <script
-        src="https://code.jquery.com/jquery-3.6.4.js"
-        integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E="
-        crossorigin="anonymous"></script>
-        <!-- <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js" integrity="sha256-xLD7nhI62fcsEZK2/v8LsBcb4lG7dgULkuXoXB/j91c=" crossorigin="anonymous"></script> -->
+    src="https://code.jquery.com/jquery-3.6.0.min.js"
+    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+    crossorigin="anonymous"></script>
     <script src="{{ asset('js/grammar.js') }}" async></script>
 @endpush
 @section('grammar')
@@ -20,34 +17,58 @@
         <div class="tense-title">Tenses</div>
         <div class="container timeline-area">
             <div class="row timeline-top">
-                <div class="col balloon-area-top past-area-top">
+                <div class="col balloon-area-top past-area-top overlap-1st">
                     <div class="balloon balloon-past-top three-lines">
-                        Past<br>Perfect<br>Progressive
+                        <div class="pulldown-title">Past<br>Perfect<br>Progressive
+                            <div class="pulldown-past pulldown-to-right">
+                                <div class="pulldown-child">Stories</div>
+                                <div class="pulldown-child">Grammar Points</div>
+                                <div class="pulldown-child">Activities</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="col"></div>
-                <div class="col balloon-area-top  past-area-top">
+                <div class="col balloon-area-top  past-area-top overlap-2nd">
                     <div class="balloon balloon-past-top two-lines">
-                        Past<br>Progressive
+                        <div class="pulldown-title">Past<br>Progressive
+                            <div class="pulldown-past pulldown-to-right">
+                                <div class="pulldown-child">Stories</div>
+                                <div class="pulldown-child">Grammar Points</div>
+                                <div class="pulldown-child">Activities</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="col"></div>
-                <div class="col balloon-area-top present-area-top">
+                <div class="col balloon-area-top present-area-top overlap-3rd">
                     <div class="balloon balloon-present-top three-lines">
-                        Present<br>Perfect<br>Progressive
+                        <div class="pulldown-title">Present<br>Perfect<br>Progressive
+                            <div class="pulldown-present pulldown-to-right">
+                                <div class="pulldown-child">Stories</div>
+                                <div class="pulldown-child">Grammar Points</div>
+                                <div class="pulldown-child">Activities</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="col"></div>
-                <div class="col balloon-area-top present-area-top">
+                <div class="col balloon-area-top present-area-top overlap-4th">
                     <div class="balloon balloon-present-top two-lines">
-                        Present<br>Progressive
+                        <div class="pulldown-title">Present<br>Progressive
+                            <div class="pulldown-present pulldown-to-left">
+                                <div class="pulldown-child">Stories</div>
+                                <div class="pulldown-child">Grammar Points</div>
+                                <div class="pulldown-child">Activities</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="col"></div>
                 <div class="col balloon-area-top future-perfect-progressive-area future-area-top">
                     <div class="balloon balloon-future-top three-lines">
                         <div class="pulldown-title">Future<br>Perfect<br>Progressive
-                            <div class="pulldown-inner">
+                            <div class="pulldown-future pulldown-to-left">
                                 <div class="pulldown-child">Stories</div>
                                 <div class="pulldown-child">Grammar Points</div>
                                 <div class="pulldown-child">Activities</div>
@@ -59,7 +80,7 @@
                 <div class="col balloon-area-top future-area-top">
                     <div class="balloon balloon-future-top two-lines">
                         <div class="pulldown-title">Future<br>Progressive
-                            <div class="pulldown-inner">
+                            <div class="pulldown-future pulldown-to-left">
                                 <div class="pulldown-child">Stories</div>
                                 <div class="pulldown-child">Grammar Points</div>
                                 <div class="pulldown-child">Activities</div>
@@ -81,7 +102,7 @@
                     <div class="col balloon-area-bottom past-perfect-area past-area-bottom overlap-1st">
                         <div class="balloon-bottom balloon-past-bottom two-lines">
                             <div class="pulldown-title">Past<br>Perfect
-                                <div class="pulldown-inner pulldown-past pulldown-to-right">
+                                <div class="pulldown-past pulldown-to-right">
                                     <div class="pulldown-child">Stories</div>
                                     <div class="pulldown-child">Grammar Points</div>
                                     <div class="pulldown-child">Activities</div>
@@ -93,7 +114,7 @@
                     <div class="col balloon-area-bottom simple-past-area  past-area-bottom overlap-2nd">
                         <div class="balloon-bottom balloon-past-bottom two-lines">
                             <div class="pulldown-title">Simple<br>Past
-                                <div class="pulldown-inner pulldown-past pulldown-to-right">
+                                <div class="pulldown-past pulldown-to-right">
                                     <div class="pulldown-child">Stories</div>
                                     <div class="pulldown-child">Grammar Points</div>
                                     <div class="pulldown-child">Activities</div>
@@ -105,7 +126,7 @@
                     <div class="col balloon-area-bottom present-area-bottom overlap-3rd">
                         <div class="balloon-bottom balloon-present-bottom two-lines">
                             <div class="pulldown-title">Present<br>Perfect
-                                <div class="pulldown-inner pulldown-present pulldown-to-right">
+                                <div class="pulldown-present pulldown-to-right">
                                     <div class="pulldown-child">Stories</div>
                                     <div class="pulldown-child">Grammar Points</div>
                                     <div class="pulldown-child">Activities</div>
@@ -117,7 +138,7 @@
                     <div class="col balloon-area-bottom present-area-bottom">
                         <div class="balloon-bottom balloon-present-bottom two-lines overlap-4th">
                             <div class="pulldown-title overlap-opposite">Simple<br>Present
-                                <div class="pulldown-inner pulldown-present pulldown-to-left">
+                                <div class="pulldown-present pulldown-to-left">
                                     <div class="pulldown-child">Stories</div>
                                     <div class="pulldown-child">Grammar Points</div>
                                     <div class="pulldown-child">Activities</div>
@@ -129,7 +150,7 @@
                     <div class="col balloon-area-bottom future-perfect-area future-area-bottom">
                         <div class="balloon-bottom balloon-future-bottom two-lines">
                             <div class="pulldown-title">Future<br>Perfect
-                                <div class="pulldown-inner pulldown-future pulldown-to-left">
+                                <div class="pulldown-future pulldown-to-left">
                                     <div class="pulldown-child">Stories</div>
                                     <div class="pulldown-child">Grammar Points</div>
                                     <div class="pulldown-child">Activities</div>
@@ -141,7 +162,7 @@
                     <div class="col balloon-area-bottom future-area-bottom">
                         <div class="balloon-bottom balloon-future-bottom two-lines">
                             <div class="pulldown-title">Simple<br>Future
-                                <div class="pulldown-inner pulldown-future pulldown-to-left">
+                                <div class="pulldown-future pulldown-to-left">
                                     <div class="pulldown-child">Stories</div>
                                     <div class="pulldown-child">Grammar Points</div>
                                     <div class="pulldown-child">Activities</div>
@@ -153,14 +174,6 @@
             </div>
         </div>
     </div>
-</div>
-
-<div class="modal js-modal">
-    <div class="modal__bg js-modal-close"></div>
-    <div class="modal__content">
-        <p>ここにモーダルウィンドウで表示したいコンテンツを入れます。モーダルウィンドウを閉じる場合は下の「閉じる」をクリックするか、背景の黒い部分をクリックしても閉じることができます。</p>
-        <a class="js-modal-close" href="">閉じる</a>
-    </div><!--modal__inner-->
 </div>
 
 @endsection
