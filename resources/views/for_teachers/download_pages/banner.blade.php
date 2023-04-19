f19753c22be71b18783178f618084386bbe958b6
Hamasaki8210 <ayakah2525@gmail.com>
2023-04-19 23:05:43 +0900

    Stories page made

---
+++ resources/views/for_teachers/download_pages/banner.blade.php
@@ -0,0 +1,9 @@
+@extends('for_teachers.main')
+@push('css')
+    <link href="{{ asset('css/download_pages/banner.css') }}" rel="stylesheet">
+@endpush
+@section('banner')
+<div class="banner">
+     
+</div>
+@endsection
