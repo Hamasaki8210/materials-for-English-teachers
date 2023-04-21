3829a8a1d21d5d5bef2ae7b131b53e70dbb5cb41
Hamasaki8210 <ayakah2525@gmail.com>
2023-04-21 21:23:48 +0900

    pdf generation in progress

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -7,11 +7,27 @@ use PDF;
 
 class PDFController extends Controller
 {
-    public function index(){
+    // public function index(){
 
-    	$pdf = PDF::loadHTML('<h1>Hello World</h1>');
+    // 	$pdf = PDF::loadHTML('<h1>Hello World</h1>');
 
-    	return $pdf->stream();
+    // 	return $pdf->stream();
 
+    // }
+    public function index() {
+        $sushiTable = [
+            'たまご' => '100円',
+            'いくら' => '200円',
+            'サーモン' => '180円',
+            'いか' => '100円',
+            'マグロ' => '110円',
+            'えび' => '100円',
+        ];
+
+        $pdf = PDF::loadView('for_teachers.grammar', compact('sushiTable'));
+        // $pdf->setPaper('A4');
+        // return $pdf->stream();
+        $pdf->setPaper('A4');
+    	return $pdf->download();
     }
 }
--- resources/views/for_teachers/download_pages/stories.blade.php
+++ resources/views/for_teachers/download_pages/stories.blade.php
@@ -9,6 +9,60 @@
 
 <div class="stories-page">
     <div class="menu">test</div>
-    <div class="article">test2</div>
+    <div class="article">
+    <style>
+            @font-face{
+                font-family: migmix;
+                font-style: normal;
+                font-weight: normal;
+                src: url("{{ storage_path('fonts/migmix-2p-regular.ttf')}}") format('truetype');
+            }
+            @font-face{
+                font-family: migmix;
+                font-style: bold;
+                font-weight: bold;
+                src: url("{{ storage_path('fonts/migmix-2p-bold.ttf')}}") format('truetype');
+            }
+            body {
+                font-family: migmix;
+                line-height: 80%;
+            }
+            .main_image {
+                width: 100%;
+                text-align: center;
+                margin: 10px 0;
+            }
+            .main_image img{
+                width: 90%;
+            }
+            .sushiTable {
+                border: 1px solid #000;
+                border-collapse: collapse;
+                width: 50%;
+            }
+            .sushiTable tr th{
+                background: #87cefa;
+                padding: 5px;
+                border: 1px solid #000;
+            }
+            .sushiTable tr td{
+                padding: 5px;
+                border: 1px solid #000;
+            }
+
+        </style>
+        PDFの出力テスト！<br>
+        <div style="font-weight:bold">ここは太字！</div>
+        <div class="main_image">
+            <img src="https://codelikes.com/wp-content/uploads/2019/06/PHP-e1601051806241.jpg" />
+        </div>
+        お寿司のテーブル
+        <table class="sushiTable">
+            <tr>
+                <th>名前</th>
+                <th>価格</th>
+            </tr>
+        </table>
+    </div>
 </div>
 @endsection
