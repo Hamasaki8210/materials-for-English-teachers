53cd96a7cd62226c0db4579440bdc2e0ddd337ee
Ayaka_I_0210 <ayakah2525@gmail.com>
2023-05-10 17:28:31 +0900

    pdf page in progress 2023/5/10

--- public/js/stories.js
+++ public/js/stories.js
@@ -1,4 +1,15 @@
 $(function(){
+
+	// when loads page, put page num
+	$(".page-num-area").remove();
+	var pageNum = $(".page").length;
+	for(var i = 1; i <= pageNum; i++){
+		var targetAppendElem = $(".page").eq(i - 1);
+		var appendNode = '<div class="page-num-area"><div class="page-num">' + i + '</div></div>';
+		targetAppendElem.after(appendNode);
+	}
+
+	// when open menu pulldown, arrow rotates
 	$('.menu-title').click(function(){
 		console.log($(this).find(".menu-title-pulldown-arrow").attr("class").indexOf("open"));
 		if($(this).find(".menu-title-pulldown-arrow").attr("class").indexOf("open") !== -1){
@@ -11,8 +22,64 @@ $(function(){
 		$(this).next().slideToggle();
 	});
 
-	$('#checkbox-2').click(function(){
-		$('.vocabulary').find('.vocabulary-title').remove();
-		$('.vocabulary').find('.vocabulary-content').remove();
+	// when clicking checkbox, session dissapears or appears
+	$('#vocabulary-1').click(function(){
+
+		$('.vocabulary-1').children('.vocabulary-title').css('display','none');
+		$('.vocabulary-1').children('.vocabulary-content').css('display','none');
+
+		// put page num in each page
+		$(".page-num-area").remove();
+		var pageNum = $(".page").length;
+		for(var i = 1; i <= pageNum; i++){
+			var targetAppendElem = $(".page").eq(i - 1);
+			var appendNode = '<div class="page-num-area"><div class="page-num">' + i + '</div></div>';
+			targetAppendElem.after(appendNode);
+		}
 	});
+
+	// when click reading slide checkbox, vocab in reading hides
+	$('.hide-switch').click(function(){
+		var eyeIcon = $(this).closest("div").children(".eye-icon");
+		if(eyeIcon.attr("class").indexOf("slider-close") != -1){
+			eyeIcon.removeClass("slider-close");
+			$(".reading-vocab-1").css('display','');
+			eyeIcon.addClass("slider-open");
+			eyeIcon.remove("i");
+			eyeIcon.html('<i class="fa fa-solid fa-eye"></i>');
+			for(var i = 0; i < $(".reading-vocab-1").length; i++){
+				$(".reading-vocab-1").eq(i).html(vocabs[i]);
+				console.log(vocabs);
+			}
+		}else{
+			eyeIcon.removeClass("slider-open");
+			eyeIcon.addClass("slider-close");
+			eyeIcon.remove("i");
+			eyeIcon.html('<i class="fa fa-solid fa-eye-slash"></i>');
+			$(".reading-vocab-1").each(function(index, element) {
+				var space = "";
+				for(var i = 0; i < element.textContent.length+3; i++){
+					space += "&nbsp;";
+				}
+				for(var i = 0; i < $(".reading-vocab-1").length; i++){
+					$(".reading-vocab-1").eq(i).html(space);
+				}
+			})
+			$(".reading-vocab-1").css('text-decoration','underline');
+		}
+	});
+
+	// if sentence too long, font size gets smaller
+	for(var i = 0; i < $(".question-sentence").length; i++){
+		if($(".question-sentence").eq(i).height() > 28.5){
+			console.log($(".question-sentence").eq(i).height());
+			$(".question-sentence").eq(i).css('font-size','18px');
+		}
+	}
+	for(var i = 0; i < $(".practice-sentence").length; i++){
+		if($(".practice-sentence").eq(i).height() > 28.5){
+			console.log($(".practice-sentence").eq(i).height());
+			$(".practice-sentence").eq(i).css('font-size','18px');
+		}
+	}
 });
--- resources/views/for_teachers/download_pages/stories.blade.php
+++ resources/views/for_teachers/download_pages/stories.blade.php
@@ -26,28 +26,45 @@
                 <span><i class="fa fa-light fa-angle-down menu-title-pulldown-arrow"></i></span>
             </p>
             <div class="pulldown-lists">
-                <ul>
-                    <li class="checkbox-lists">
-                        <input type="checkbox" id="checkbox-1" checked>
+                <div class="checkbox-lists">
+                    <div class="menu-session-outer">
+                        <input class="menu-sessions" type="checkbox" id="checkbox-1" checked>
                         <label for="checkbox-1">Reading</label>
-                    </li>
-                    <li class="checkbox-lists">
-                        <input type="checkbox" id="checkbox-2" checked>
-                        <label for="checkbox-2">Vocabulary</label>
-                    </li>
-                    <li class="checkbox-lists">
-                        <input type="checkbox" id="checkbox-3" checked>
+                    </div>
+                    <div class="reading-visible-area">
+                        <label class="switch" for="slider-1">
+                            <input type="checkbox" id="slider-1" class="hide-switch" checked/>
+                            <div class="slider round"></div>
+                        </label>
+                        <div class="eye-icon">
+                            <i class="fa fa-solid fa-eye"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="checkbox-lists">
+                    <div class="menu-session-outer">
+                        <input class="menu-sessions" type="checkbox" id="vocabulary-1" checked>
+                        <label for="vocabulary-1">Vocabulary</label>
+                    </div>
+                </div>
+                <div class="checkbox-lists">
+                    <div class="menu-session-outer">
+                        <input class="menu-sessions" type="checkbox" id="checkbox-3" checked>
                         <label for="checkbox-3">Questions</label>
-                    </li>
-                    <li class="checkbox-lists">
-                        <input type="checkbox" id="checkbox-4" checked>
+                    </div>
+                </div>
+                <div class="checkbox-lists">
+                    <div class="menu-session-outer">
+                        <input class="menu-sessions" type="checkbox" id="checkbox-4" checked>
                         <label for="checkbox-4">Practice</label>
-                    </li>
-                    <li class="checkbox-lists">
-                        <input type="checkbox" id="checkbox-5" checked>
-                        <label for="checkbox-5">Answers</label>
-                    </li>
-                </ul>
+                    </div>
+                </div>
+                <div class="checkbox-lists">
+                    <div class="menu-session-outer">
+                        <input class="menu-sessions" type="checkbox" id="checkbox-5" checked>
+                        <label for="checkbox-5  ">Answers</label>
+                    </div>
+                </div>
             </div>
             <div>
                 <p class="menu-title">
@@ -56,26 +73,26 @@
                 </p>
                 <div class="pulldown-lists">
                     <ul>
-                        <li class="checkbox-lists">
-                            <input type="checkbox" id="checkbox-1" checked>
+                        <div class="checkbox-lists">
+                            <input class="menu-sessions" type="checkbox" id="checkbox-1" checked>
                             <label for="checkbox-1">Reading</label>
-                        </li>
-                        <li class="checkbox-lists">
-                            <input type="checkbox" id="checkbox-2" checked>
+                        </div>
+                        <div class="checkbox-lists">
+                            <input class="menu-sessions" type="checkbox" id="checkbox-2" checked>
                             <label for="checkbox-2">Vocabulary</label>
-                        </li>
-                        <li class="checkbox-lists">
-                            <input type="checkbox" id="checkbox-3" checked>
+                        </div>
+                        <div class="checkbox-lists">
+                            <input class="menu-sessions" type="checkbox" id="checkbox-3" checked>
                             <label for="checkbox-3">Questions</label>
-                        </li>
-                        <li class="checkbox-lists">
-                            <input type="checkbox" id="checkbox-4" checked>
+                        </div>
+                        <div class="checkbox-lists">
+                            <input class="menu-sessions" type="checkbox" id="checkbox-4" checked>
                             <label for="checkbox-4">Practice</label>
-                        </li>
-                        <li class="checkbox-lists">
-                            <input type="checkbox" id="checkbox-5" checked>
+                        </div>
+                        <div class="checkbox-lists">
+                            <input class="menu-sessions" type="checkbox" id="checkbox-5" checked>
                             <label for="checkbox-5">Answers</label>
-                        </li>
+                        </div>
                     </ul>
                 </div>
             </div>
@@ -83,7 +100,7 @@
         <div class="content-area">
             <div class="lesson-separator"></div>
             <div class="indivisual-content-area">
-                <div class="page1">
+                <div class="page">
                     <div class="reading">
                         <div class="reading-title">
                             Tim's Busy Day
@@ -92,22 +109,22 @@
                             <div class="reading-story">
                                 <img class="reading-pic" src="{{ asset('images/soccer-sample.jpg') }}" alt="">
                                 There is a boy named Tim who lives in a
-                                small <b>village</b>. He wakes up early every
+                                small <b class="reading-vocab-1">village</b>. He wakes up early every
                                 morning and goes to school. Today, he has
-                                a <b>science</b> test, and he feels excited about
+                                a <b class="reading-vocab-1">science</b> test, and he feels excited about
                                 it. During the test, he carefully reads the
                                 questions and writes his answers. He
                                 finishes before everyone else and feels
-                                <b>confident</b> about his <b>performance</b>. After school, he goes to the
+                                <b class="reading-vocab-1">confident</b> about his <b class="reading-vocab-1">performance</b>. After school, he goes to the
                                 park to play soccer with his friends. They have a lot of fun, and
                                 Tim even scores a goal! When he gets home, his mom makes his
                                 favorite dinner, spaghetti. He eats quickly because he wants to
                                 finish his homework before bedtime. Tim feels tired but happy
-                                after a <b>productive</b> day.
+                                after a <b class="reading-vocab-1">productive</b> day.
                             </div>
                         </div>
                     </div>
-                    <div class="vocabulary">
+                    <div class="vocabulary-1">
                         <div class="vocabulary-title">
                             Vocabulary
                         </div>
@@ -120,30 +137,122 @@
                         </div>
                     </div>
                 </div>
-                <div class="page-num-area">
-                    <div class="margin-top"></div>
-                    <div class="page-num">1</div>
-                </div>
                 <div class="page-separator"></div>
-                <div class="page2">
-                    <div>
-                        <div class="question-title">
-                            Questions
-                        </div>
-                        <div class="question-content">
-                            <div class="question"><span>1.&nbsp;&nbsp;</span>What does Tim do every morning?</div>
-                            <div class="question"><span>2.&nbsp;&nbsp;</span>How does Tim feel about his science test?</div>
-                            <div class="question"><span>3.&nbsp;&nbsp;</span>What does Tim do during the test?</div>
-                            <div class="question"><span>4.&nbsp;&nbsp;</span>How does Tim feel after finishing the test?</div>
-                            <div class="question"><span>5.&nbsp;&nbsp;</span>What does Tim do after school?</div>
-                            <div class="question"><span>5.&nbsp;&nbsp;</span>What game do Tim and his friends play at the park?</div>
-                            <div class="question"><span>5.&nbsp;&nbsp;</span>Why does Tim eat quickly?</div>
-                            <div class="question"><span>5.&nbsp;&nbsp;</span>How does Tim feel after finishing his homework?</div>
-                        </div>
+                <div class="page">
+                    <div class="question-title">
+                        Questions
+                    </div>
+                    <div class="question">
+                        <div class="question-sentence"><span>1.&nbsp;&nbsp;</span>What does Tim do every morning?</div>
+                        <div class="question-answer"></div>
+                    </div>
+                    <div class="question">
+                        <div class="question-sentence"><span>2.&nbsp;&nbsp;</span>How does Tim feel about his science test?</div>
+                        <div class="question-answer"></div>
+                    </div>
+                    <div class="question">
+                        <div class="question-sentence"><span>3.&nbsp;&nbsp;</span>What does Tim do during the test?</div>
+                        <div class="question-answer"></div>
+                    </div>
+                    <div class="question">
+                        <div class="question-sentence"><span>4.&nbsp;&nbsp;</span>How does Tim feel after finishing the test?</div>
+                        <div class="question-answer"></div>
+                    </div>
+                    <div class="question">
+                        <div class="question-sentence"><span>5.&nbsp;&nbsp;</span>What does Tim do after school?</div>
+                        <div class="question-answer"></div>
+                    </div>
+                    <div class="question">
+                        <div class="question-sentence"><span>6.&nbsp;&nbsp;</span>What game do Tim and his friends play at the park?</div>
+                        <div class="question-answer"></div>
+                    </div>
+                    <div class="question">
+                        <div class="question-sentence"><span>7.&nbsp;&nbsp;</span>Why does Tim eat quickly?</div>
+                        <div class="question-answer"></div>
+                    </div>
+                    <div class="question">
+                        <div class="question-sentence"><span>8.&nbsp;&nbsp;</span>How does Tim feel after finishing his homework?</div>
+                        <div class="question-answer"></div>
                     </div>
                 </div>
+                <div class="page-separator"></div>
+                <div class="page">
+                    <div class="practice-title">
+                        Practice
+                    </div>
+                    <div class="practice">
+                        <div class="practice-sentence"><span>1.&nbsp;&nbsp;</span>Do you usually wake up early in the morning like Tim?</div>
+                        <div class="practice-answer"></div>
+                    </div>
+                    <div class="practice">
+                        <div class="practice-sentence"><span>2.&nbsp;&nbsp;</span>How do you feel when you have a test at school?</div>
+                        <div class="practice-answer"></div>
+                    </div>
+                    <div class="practice">
+                        <div class="practice-sentence"><span>3.&nbsp;&nbsp;</span>Do you carefully read the practices during a test like Tim does?</div>
+                        <div class="practice-answer"></div>
+                    </div>
+                    <div class="practice">
+                        <div class="practice-sentence"><span>4.&nbsp;&nbsp;</span>How do you feel when you finish a test before everyone else?</div>
+                        <div class="practice-answer"></div>
+                    </div>
+                    <div class="practice">
+                        <div class="practice-sentence"><span>5.&nbsp;&nbsp;</span>Do you like to play sports with your friends after school like Tim does?</div>
+                        <div class="practice-answer"></div>
+                    </div>
+                    <div class="practice">
+                        <div class="practice-sentence"><span>6.&nbsp;&nbsp;</span>What is your favorite game to play with your friends?</div>
+                        <div class="practice-answer"></div>
+                    </div>
+                    <div class="practice">
+                        <div class="practice-sentence"><span>7.&nbsp;&nbsp;</span>What is your favorite food that your mom makes for dinner?</div>
+                        <div class="practice-answer"></div>
+                    </div>
+                    <div class="practice">
+                        <div class="practice-sentence"><span>8.&nbsp;&nbsp;</span>How do you feel after finishing your homework?</div>
+                        <div class="practice-answer"></div>
+                    </div>
+                </div>
+                <div class="page-separator"></div>
+                <div class="page">
+                    <div class="answers-title">
+                        Answers
+                    </div>
+                    <table>
+                        <tr class="answer">
+                            <td class="answer-num">1.&nbsp;</td><td class="answer-sentence">Tim wakes up early every morning and goes to school.</td>
+                        </tr>
+                        <tr class="answer">
+                            <td class="answer-num">2.&nbsp;</td><td class="answer-sentence">Tim feels excited about his science test.</td>
+                        </tr>
+                        <tr class="answer">
+                            <td class="answer-num">3.&nbsp;</td><td class="answer-sentence">During the test, Tim carefully reads the questions and writes his answers.</td>
+                        </tr>
+                        <tr class="answer">
+                            <td class="answer-num">4.&nbsp;</td><td class="answer-sentence">Tim feels confident about his performance after finishing the test.</td>
+                        </tr>
+                        <tr class="answer">
+                            <td class="answer-num">5.&nbsp;</td><td class="answer-sentence">After school, Tim goes to the park to play soccer with his friends.</td>
+                        </tr>
+                        <tr class="answer">
+                            <td class="answer-num">6.&nbsp;</td><td class="answer-sentence">Tim and his friends play soccer at the park.</td>
+                        </tr>
+                        <tr class="answer">
+                            <td class="answer-num">7.&nbsp;</td><td class="answer-sentence">Tim's mom makes his favorite dinner, spaghetti.</td>
+                        </tr>
+                        <tr class="answer">
+                            <td class="answer-num">8.&nbsp;</td><td class="answer-sentence">Tim eats quickly because he wants to finish his homework before bedtime.</td>
+                        </tr>
+                        <tr class="answer">
+                            <td class="answer-num">9.&nbsp;</td><td class="answer-sentence">Tim feels tired but happy after finishing his homework.</td>
+                        </tr>
+                        <tr class="answer">
+                            <td class="answer-num">10.&nbsp;</td><td class="answer-sentence">Tim feels productive and happy at the end of the day.</td>
+                        </tr>
+                    </table>
+                </div>
             </div>
-        <div class="page-num">2</div>
+        </div>
     </div>
 </div>
 @endsection
